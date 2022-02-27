6ed8eced5a8c4071e5f849d4c8b3c832d95b431c
bezdjian <bezdjian-370z@hotmail.com>
2022-02-27 23:56:31 +0100

    Change region, ec2 type and add Name tags

--- terraform/base_web_app/main.tf
+++ terraform/base_web_app/main.tf
@@ -5,7 +5,7 @@
 provider "aws" {
   access_key = "ACCESS_KEY"
   secret_key = "SECRET_KEY"
-  region     = "us-east-1"
+  region     = "eu-north-1"
 }
 
 ##################################################################################
@@ -24,11 +24,17 @@ data "aws_ssm_parameter" "ami" {
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = "true"
+  tags = {
+    Name = "Terraform_VPC"
+  }
 
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "Terraform_IGW"
+  }
 
 }
 
@@ -36,11 +42,17 @@ resource "aws_subnet" "subnet1" {
   cidr_block              = "10.0.0.0/24"
   vpc_id                  = aws_vpc.vpc.id
   map_public_ip_on_launch = "true"
+  tags = {
+    Name = "Terraform_Subnet"
+  }
 }
 
 # ROUTING #
 resource "aws_route_table" "rtb" {
   vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "Terraform_RTB"
+  }
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -79,17 +91,17 @@ resource "aws_security_group" "nginx-sg" {
 # INSTANCES #
 resource "aws_instance" "nginx1" {
   ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
 
   user_data = <<EOF
-#! /bin/bash
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
+                #! /bin/bash
+                sudo amazon-linux-extras install -y nginx1
+                sudo service nginx start
+                sudo rm /usr/share/nginx/html/index.html
+                echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+              EOF
 
 }
 
